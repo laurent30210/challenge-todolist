f8033dc4ec9369b72ca6c244ffc1ff63e46e3113
Laurent30210 <fligbook@yahoo.fr>
2021-01-26 19:31:45 +0100

    BASE installed

---
+++ FRONT/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__title">todolist</h1>
+    </div>
+  );
+}
+
+export default App;
